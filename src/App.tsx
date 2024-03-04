a30a40e2f51ef747d28d58731f2ae45d2481d158
Maxim <schubert.walk@yandex.ru>
2024-03-04 14:48:59 +0300

    add styles and themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Suspense } from "react";
-import "./index.scss";
+import "./styles/index.scss";
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme(); //хук для темы
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Spinner//</div>}>
