7f15582428c386e14367a77bd68f08e79b8caa99
Maxim <schubert.walk@yandex.ru>
2024-03-05 13:16:55 +0300

    create nav-bar
    Cделал виджет - навигационную панель, в которой находятся унифицированные ссылки из ui-kit, которые можно легко кастомизировать (уже применяются две заранее созданные темы)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
 import "./styles/index.scss";
-import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme(); //хук для темы
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE THEME</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
     </div>
   );
 };
---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,35 @@
+import { Link, LinkProps } from "react-router-dom";
+import { FC } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./AppLink.module.scss";
+
+export enum AppLinkTheme {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+}
+
+// Расширяемся от типа Link (+to, +children)
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = (props) => {
+  const {
+    className,
+    to,
+    children,
+    theme = AppLinkTheme.PRIMARY,
+    ...otherProps
+  } = props;
+
+  return (
+    <Link
+      to={to}
+      className={classNames(cls.AppLink, {}, [className, cls[theme]])} //cls['secondary']
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
