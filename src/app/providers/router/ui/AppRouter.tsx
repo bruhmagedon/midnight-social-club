43807b0f71b524928e59e49ff6403f166f5a645f
Maxim <schubert.walk@yandex.ru>
2024-03-10 14:36:24 +0300

    add jest / routes / loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 const AppRouter = () => {
     return (
         <>
             {/* eslint-disable-next-line i18next/no-literal-string */}
-            <Suspense fallback={<div>Spinner//</div>}>
+            <Suspense fallback={<PageLoader />}>
                 <Routes>
                     {/* Создаем роутер из конфига-маршрутов */}
                     {Object.values(routeConfig).map(({ path, element }) => (
