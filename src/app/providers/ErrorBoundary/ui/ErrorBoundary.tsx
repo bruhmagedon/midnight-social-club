1e06ca53514de1547c77ca5740f515a51ea0fb0b
Maxim <schubert.walk@yandex.ru>
2024-03-10 15:19:26 +0300

    add error-boundary
    Добавил ErrorBoundary, который будет отлавливать ошибки на всем сайте
    Для него создал компонент PageError. оповещающий об ошибке

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import { Component, Suspense } from 'react';
+import { PageError } from 'widgets/PageError/ui/PageError';
+
+interface ErrorBoundaryProps {
+    children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    render() {
+        const { children } = this.props;
+        const { hasError } = this.state;
+
+        if (hasError) {
+            return (
+                <Suspense fallback="">
+                    <PageError />
+                </Suspense>
+            );
+        }
+
+        return children;
+    }
+}
+
+export default ErrorBoundary;
