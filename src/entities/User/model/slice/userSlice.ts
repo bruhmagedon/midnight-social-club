f48549dd0b8990080dd7fb164a8f2f6b5905c775
bruhmagedon <schubert.walk@yandex.ru>
2024-06-10 19:05:21 +0300

    feat: Добавлена сущность User
    С помощью User проверяется авторизация пользователя в Redux, она используется в фиче LoginSlice

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,12 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { type UserSchema } from '../types/user';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { User, type UserSchema } from '../types/user';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
 
 const initialState: UserSchema = {};
 
+// Сущность отвечающая за то авторизован ли пользователь или нет
 export const userSlice = createSlice({
     name: 'user',
     initialState,
-    reducers: {},
+    reducers: {
+        // Авторизация
+        setAuthData: (state, action: PayloadAction<User>) => {
+            state.authData = action.payload;
+        },
+        // Проверка авторизации
+        initAuthData: state => {
+            const user = localStorage.getItem(USER_LOCALSTORAGE_KEY);
+            if (user) {
+                state.authData = JSON.parse(user);
+            }
+        },
+        logout: state => {
+            state.authData = undefined;
+            localStorage.removeItem(USER_LOCALSTORAGE_KEY);
+        },
+    },
 });
 
 export const { actions: userActions } = userSlice;
