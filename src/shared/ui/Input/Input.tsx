7ce12437a8620cd2dfab4f35fcbbc98bab53f7ad
bruhmagedon <schubert.walk@yandex.ru>
2024-06-10 11:16:07 +0300

    feat: Cоздал кастомный input

---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,86 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Input.module.scss';
+import { InputHTMLAttributes, memo, useEffect, useRef, useState } from 'react';
+
+// Омит чтобы использовать свои value и onChange
+type HTMLInputProps = Omit<
+    InputHTMLAttributes<HTMLInputElement>,
+    'value' | 'onChange'
+>;
+
+interface InputProps extends HTMLInputProps {
+    className?: string;
+    value?: string;
+    onChange?: (value: string) => void;
+    autofocus?: boolean;
+}
+
+export const Input = memo((props: InputProps) => {
+    const {
+        className,
+        value,
+        autofocus,
+        onChange,
+        type = 'text',
+        placeholder,
+        ...otherProps
+    } = props;
+
+    const [isFocused, setIsFocused] = useState(false);
+    const [caretPosition, setCaretPosition] = useState(0);
+
+    const ref = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        if (autofocus) {
+            setIsFocused(true);
+            ref.current?.focus();
+        }
+    }, [autofocus]);
+
+    const onBlur = () => {
+        setIsFocused(false);
+    };
+
+    const onFocus = () => {
+        setIsFocused(true);
+    };
+
+    const onSelect = (e: any) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        setCaretPosition(e?.target?.selectionStart || 0);
+    };
+
+    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // Опциональная цепочка для функций
+        onChange?.(e.target.value);
+        setCaretPosition(e.target.value.length);
+    };
+
+    return (
+        <div className={classNames(cls.InputWrapper, {}, [className])}>
+            {/* Если нет placeholder то ничего не показываем */}
+            {placeholder && (
+                <div className={cls.placeholder}>{`${placeholder}>`}</div>
+            )}
+            <div className={cls.caretWrapper}>
+                <input
+                    ref={ref}
+                    type={type}
+                    value={value}
+                    onChange={onChangeHandler}
+                    className={cls.input}
+                    onBlur={onBlur}
+                    onFocus={onFocus}
+                    onSelect={onSelect}
+                    {...otherProps}
+                />
+                {isFocused && (
+                    <span
+                        className={cls.caret}
+                        style={{ left: `${caretPosition * 9}px` }}
+                    />
+                )}
+            </div>
+        </div>
+    );
+});
