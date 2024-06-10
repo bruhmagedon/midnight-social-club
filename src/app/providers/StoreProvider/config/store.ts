e9e23af90964d609f04c2af48216e00cf144a569
bruhmagedon <schubert.walk@yandex.ru>
2024-06-10 19:04:00 +0300

    feat: :rotating_light: Добавлена фича авторизации
    LoginSchema для типа LoginForm в Стор; LoginSlice для передачи данных авторизации в Store; ReduxThunk loginByUsername для запросов к серверу для авторизации; Связан LoginForm с redux-функционалов (select/dispatch)

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,9 +1,13 @@
 import { ReducersMapObject, configureStore } from '@reduxjs/toolkit';
 import type { StateSchema } from './StateSchema';
 import { userReducers } from 'entities/User';
+import { loginReducers } from 'features/AuthByUsername';
 
 export function createReduxStore(initialState?: StateSchema) {
-    const rootReducers: ReducersMapObject<StateSchema> = { user: userReducers };
+    const rootReducers: ReducersMapObject<StateSchema> = {
+        user: userReducers,
+        loginForm: loginReducers,
+    };
 
     return configureStore<StateSchema>({
         reducer: rootReducers,
