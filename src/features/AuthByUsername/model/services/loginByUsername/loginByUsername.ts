fcb6a412a771ffc26cdf8d068d11ee306d74ac06
bruhmagedon <schubert.walk@yandex.ru>
2024-06-17 12:00:56 +0300

    feat: Новая фича: Авторизация пользователя
    Использует сущность User; thunk для запроса к серверу; Селекторы для каждого поля; UI формы входа в модальном окне

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { User, userActions } from 'entities/User';
-import i18n from 'shared/config/i18n/i18n';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
 
 interface LoginByUsernameProps {
@@ -34,6 +33,6 @@ export const loginByUsername = createAsyncThunk<
 
         return response.data;
     } catch (e) {
-        return thunkAPI.rejectWithValue(i18n.t('Некорректная авторизация'));
+        return thunkAPI.rejectWithValue('error');
     }
 });
