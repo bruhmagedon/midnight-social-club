a0293ed35334db0918e9b0f2e19681be2edcd6f4
bruhmagedon <schubert.walk@yandex.ru>
2024-07-08 16:32:31 +0300

    perf: Доп memo для оптимизации компонентов

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,14 @@
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './MainPage.module.scss';
+import { memo } from 'react';
 
-const MainPage = () => {
+const MainPage = memo(() => {
     const { t } = useTranslation();
 
     return (
         <div className={classNames(cls.MainPage)}>{t('Главная страница')}</div>
     );
-};
+});
 
 export default MainPage;
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,5 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { ButtonHTMLAttributes, FC } from 'react';
+import { ButtonHTMLAttributes, memo } from 'react';
 import cls from './Button.module.scss';
 
 export enum ButtonTheme {
@@ -19,9 +19,10 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     theme?: ButtonTheme;
     square?: boolean;
     size?: ButtonSize;
+    children?: React.ReactNode;
 }
 
-export const Button: FC<ButtonProps> = props => {
+export const Button = memo((props: ButtonProps) => {
     const {
         className,
         children,
@@ -45,4 +46,4 @@ export const Button: FC<ButtonProps> = props => {
             {children}
         </button>
     );
-};
+});
