eb2d3bc579977b274c30792ec9f10e622c8496da
bruhmagedon <schubert.walk@yandex.ru>
2024-09-21 08:56:02 +0300

    build: Поянения к конфигу

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -3,10 +3,10 @@ import { type BuildOptions } from './types/config';
 
 export function buildResolvers(options: BuildOptions): ResolveOptions {
     return {
-        extensions: ['.tsx', '.ts', '.js'],
-        preferAbsolute: true,
-        modules: [options.paths.src, 'node_modules'],
-        mainFiles: ['index'],
-        alias: {},
+        extensions: ['.tsx', '.ts', '.js'], // Расширения файлов, которые webpack пропустит при импорте модуля
+        preferAbsolute: true, // Приоритет на абслютные пути при импорте модулей
+        modules: [options.paths.src, 'node_modules'], // TODO???
+        mainFiles: ['index'], // TODO???
+        alias: {}, // TODO???
     };
 }
