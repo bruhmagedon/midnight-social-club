90be30ee123548cea4bbc59a7fefae3c96af5524
bruhmagedon <schubert.walk@yandex.ru>
2024-10-08 11:35:34 +0300

    feat: фича авторизации, изолированная (по fsd), наружу выходить только модалка и подключения к стору

---
+++ src/features/AuthByUsername/model/slice/loginSlice.tsx
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from 'entities/User';
+import { LoginSchema } from '../types/loginSchema';
+import { loginByUsername } from '../services/loginByUsername/loginByUsername';
+
+const initialState: LoginSchema = {
+    username: '',
+    password: '',
+    isLoading: false,
+};
+
+export const loginSlice = createSlice({
+    name: 'login',
+    initialState,
+    reducers: {
+        setUsername: (state, actions: PayloadAction<string>) => {
+            state.username = actions.payload;
+        },
+        setPassword: (state, actions: PayloadAction<string>) => {
+            state.password = actions.payload;
+        },
+    },
+    // Коннект с AsyncThunk
+    extraReducers: (builder) => {
+        builder
+            .addCase(loginByUsername.pending, (state) => {
+                state.isLoading = true;
+                state.error = undefined;
+            })
+            .addCase(loginByUsername.fulfilled, (state, action) => {
+                state.isLoading = false;
+            })
+            .addCase(loginByUsername.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
+});
+
+export const { actions: loginActions } = loginSlice;
+export const { reducer: loginReducer } = loginSlice;
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,20 +1,66 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
+import { useDispatch, useSelector } from 'react-redux';
+import { memo, useCallback } from 'react';
+import { Text, TextTheme } from 'shared/ui/Text/Text';
+import { getLoginState } from '../../model/selectors/getLoginState/getLoginState';
+import { loginByUsername } from '../../model/services/loginByUsername/loginByUsername';
+import { loginActions } from '../../model/slice/loginSlice';
 import cls from './LoginForm.module.scss';
 
 interface LoginFormProps {
   className?: string;
 }
 
-export const LoginForm = ({ className }: LoginFormProps) => {
+export const LoginForm = memo(({ className }: LoginFormProps) => {
     const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const {
+        username, password, error, isLoading,
+    } = useSelector(getLoginState);
+
+    const onChangeUsername = useCallback((value: string) => {
+        dispatch(loginActions.setUsername(value));
+    }, [dispatch]);
+
+    const onChangePassword = useCallback((value: string) => {
+        dispatch(loginActions.setPassword(value));
+    }, [dispatch]);
+
+    const onLoginClick = useCallback(() => {
+        // Вызов AsyncThunk
+        dispatch(loginByUsername({ username, password }));
+    }, [dispatch, password, username]);
+
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
-            <Input autofocus type="text" className={cls.input} placeholder={t('Введите username')} />
-            <Input type="text" className={cls.input} placeholder={t('Введите пароль')} />
-            <Button className={cls.loginBtn}>{t('Войти')}</Button>
+            <Text title={t('Форма авторизации')} />
+            {error && <Text theme={TextTheme.ERROR} text={error} />}
+            <Input
+                autofocus
+                type="text"
+                className={cls.input}
+                placeholder={t('Введите username')}
+                onChange={onChangeUsername}
+                value={username}
+            />
+            <Input
+                type="text"
+                className={cls.input}
+                placeholder={t('Введите пароль')}
+                onChange={onChangePassword}
+                value={password}
+            />
+            <Button
+                onClick={onLoginClick}
+                theme={ButtonTheme.OUTLINE}
+                className={cls.loginBtn}
+                disabled={isLoading}
+            >
+                {t('Войти')}
+            </Button>
         </div>
     );
-};
+});
