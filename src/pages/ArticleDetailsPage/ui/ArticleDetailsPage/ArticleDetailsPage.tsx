1bb916c05c1362a2e83d5c1f23861828143b5bf5
bruhmagedon <schubert.walk@yandex.ru>
2025-03-16 09:47:07 +0300

    feat: страница детализации статей и скелетоны

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { ArticleDetails } from '_entities/Article';
+import { useParams } from 'react-router-dom';
 import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
@@ -9,12 +10,20 @@ interface ArticleDetailsPageProps {
 }
 
 const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
-    const { t } = useTranslation('article');
+    const { t } = useTranslation('article-details');
+    const { id } = useParams<{id: string}>();
+
+    if (!id) {
+        return (
+            <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+                {t('Статья не найдена')}
+            </div>
+        );
+    }
 
     return (
-        // eslint-disable-next-line i18next/no-literal-string
         <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-            <ArticleDetails />
+            <ArticleDetails id={id} />
         </div>
     );
 };
